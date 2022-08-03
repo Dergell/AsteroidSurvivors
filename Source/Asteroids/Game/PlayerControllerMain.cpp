a2e9c65a2b0425a24aefd4d51b9d0eb3fc3d9b8a
Tony Schmich <tonyschmich@gmail.com>
2022-08-03 21:35:10 +0200

    feat: add a custom cursor on the game plane

--- Source/Asteroids/Game/PlayerControllerMain.cpp
+++ Source/Asteroids/Game/PlayerControllerMain.cpp
@@ -3,27 +3,59 @@
 #include "PlayerControllerMain.h"
 
 #include "PlayerShip.h"
+#include "Engine/StaticMeshActor.h"
+
+APlayerControllerMain::APlayerControllerMain()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
 
 void APlayerControllerMain::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 
 	float MousePosX, MousePosY;
-	GetMousePosition(MousePosX, MousePosY);
+	if (GetMousePosition(MousePosX, MousePosY))
+	{
+		FVector TraceLocation, TraceDirection;
+		DeprojectScreenPositionToWorld(MousePosX, MousePosY, TraceLocation, TraceDirection);
+		const FVector MousePosPlane = FMath::LinePlaneIntersection(TraceLocation,
+		                                                           TraceLocation + TraceDirection * 10000,
+		                                                           FPlane(0, 0, 1, 0));
 
-	FVector TraceLocation, TraceDirection;
-	DeprojectScreenPositionToWorld(MousePosX, MousePosY, TraceLocation, TraceDirection);
-	const FVector MousePosPlane = FMath::LinePlaneIntersection(TraceLocation,
-	                                                           TraceLocation + TraceDirection * 10000,
-	                                                           FPlane(0, 0, 1, 0));
+		if (APlayerShip* PlayerShip = Cast<APlayerShip>(GetPawn()))
+		{
+			PlayerShip->Turn(MousePosPlane, DeltaSeconds);
 
-	if (APlayerShip* PlayerShip = Cast<APlayerShip>(GetPawn()))
-		PlayerShip->Turn(MousePosPlane, DeltaSeconds);
+			const FVector CursorVector = MousePosPlane - PlayerShip->GetActorLocation();
+			CursorActor->SetActorRotation(CursorVector.Rotation() + FRotator(0, 90, 0));
+		}
+
+		CursorActor->SetActorLocation(MousePosPlane);
+	}
 }
 
 void APlayerControllerMain::UpdateScore_Implementation(int32 Points)
 {
 	MainWidget->UpdateScore(Points);
+}
+
+void APlayerControllerMain::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (CursorClass != nullptr)
+	{
+		FActorSpawnParameters SpawnParams;
+		SpawnParams.Owner = this;
+		CursorActor = GetWorld()->SpawnActor<AStaticMeshActor>(CursorClass, FVector::ZeroVector,
+		                                                       FRotator::ZeroRotator, SpawnParams);
+	}
+	else
+	{
+		UE_LOG(LogBlueprint, Warning, TEXT("Could not spawn CursorActor. Make sure to select a CursorClass."));
+	}
 }
 
 void APlayerControllerMain::OnPossess(APawn* aPawn)
--- Source/Asteroids/Game/PlayerControllerMain.h
+++ Source/Asteroids/Game/PlayerControllerMain.h
@@ -8,6 +8,8 @@
 #include "WidgetMain.h"
 #include "PlayerControllerMain.generated.h"
 
+class AStaticMeshActor;
+
 /**
  * Class which implements the main player controller. Extended by blueprints.
  */
@@ -17,6 +19,7 @@ class ASTEROIDS_API APlayerControllerMain : public APlayerController, public IIt
 	GENERATED_BODY()
 
 public:
+	APlayerControllerMain();
 	virtual void Tick(float DeltaSeconds) override;
 
 	// Interface called when score needs to be updated
@@ -25,10 +28,19 @@ public:
 	UPROPERTY(EditDefaultsOnly)
 	TSubclassOf<UWidgetMain> MainWidgetClass;
 
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AStaticMeshActor> CursorClass;
+
 private:
 	// Called when the controller possesses a pawn
 	virtual void OnPossess(APawn* aPawn) override;
 
 	UPROPERTY()
 	UWidgetMain* MainWidget;
+	UPROPERTY()
+	AStaticMeshActor* CursorActor;
 };
