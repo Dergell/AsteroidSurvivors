d2b2cd46f01882f85b31f75042c604858d6db2d5
Tony Schmich <tonyschmich@gmail.com>
2022-11-23 20:00:54 +0100

    chore: Compatibility Update for Unreal Engine 5.1

--- Source/AsteroidsEditor.Target.cs
+++ Source/AsteroidsEditor.Target.cs
@@ -9,6 +9,7 @@ public class AsteroidsEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
 		ExtraModuleNames.AddRange( new string[] { "Asteroids", "AsteroidsEditor" } );
 	}
