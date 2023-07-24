a9fbc99af6cb599b90e266965d0744c0f238facc
Tony Schmich <tonyschmich@gmail.com>
2023-07-24 20:03:43 +0200

    feat: add game time to hud

--- Source/AsteroidSurvivors/UI/PlayerWidget.cpp
+++ Source/AsteroidSurvivors/UI/PlayerWidget.cpp
@@ -1,3 +1,12 @@
 // Asteroid Survivors - Copyright (C) 2023 Tony Schmich
 
 #include "PlayerWidget.h"
+
+void UPlayerWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	const int32 TimeSeconds = GetWorld()->GetTimeSeconds();
+	const int32 TimeMinutes = floor(TimeSeconds / 60);
+	TextTimer->SetText(FText::FromString(FString::Printf(TEXT("%02d:%02d"), TimeMinutes % 60, TimeSeconds % 60)));
+}
--- Source/AsteroidSurvivors/UI/PlayerWidget.h
+++ Source/AsteroidSurvivors/UI/PlayerWidget.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
 #include "PlayerWidget.generated.h"
 
 /**
@@ -15,7 +16,13 @@ class ASTEROIDSURVIVORS_API UPlayerWidget : public UUserWidget
 	GENERATED_BODY()
 
 public:
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
 	// Actions
 	UFUNCTION(BlueprintImplementableEvent)
 	void UpdateScore(int32 Score);
+
+protected:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> TextTimer;
 };
